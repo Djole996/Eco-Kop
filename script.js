8e30e5f99100d33645221140264f3ff207d649bf
Djole996 <46134487+Djole996@users.noreply.github.com>
2022-05-27 14:16:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+                  
+                  
+                 /* document.addEventListener('DOMContentLoaded', function() {
+                     var button = document.createElement('button');
+                      button.type = 'button';
+                         button.innerHTML = 'Datum prijavljivanja:';
+                          button.className = 'btn-styled';
+ 
+                             button.onclick = function Prijavljivanje() {
+
+                                                         };
+ 
+                        var container = document.getElementById('prijavljivanje');
+                        container.appendChild(button);
+                        }, false)
+
+                            
+                        document.addEventListener('DOMContentLoaded', function() {
+                     var button = document.createElement('button');
+                      button.type = 'button';
+                         button.innerHTML = 'Datum odjavljivanja:';
+                          button.className = 'btn-styled';
+ 
+                             button.onclick = function Odjavljivanje() {
+
+                                                         };
+ 
+                        var container = document.getElementById('odjavljivanje');
+                        container.appendChild(button);
+                        }, false)
+
+
+                        document.addEventListener('DOMContentLoaded', function() {
+                     var button = document.createElement('button');
+                      button.type = 'button';
+                         button.innerHTML = 'Broj osoba:';
+                          button.className = 'btn-styled';
+ 
+                             button.onclick = function BrojOSoba() {
+
+                                                         };
+ 
+                        var container = document.getElementById('broj-osoba');
+                        container.appendChild(button);
+                        }, false)
+
+
+                        document.addEventListener('DOMContentLoaded', function() {
+                     var button = document.createElement('button');
+                      button.type = 'button';
+                         button.innerHTML = 'Trazi';
+                          button.className = 'btn-styled';
+ 
+                             button.onclick = function Trazi() {
+
+                                                         };
+ 
+                        var container = document.getElementById('container4');
+                        container.appendChild(button);
+                        }, false) */
+
+                       
