0e5aa6e984baa6c73cade0b6969a0915050623ad
unknown <crvenioklop@gmail.com>
2022-08-20 19:07:31 +0200

    add modals

--- apps.js
+++ apps.js
@@ -1,20 +1,48 @@
-///galerija
-document.addEventListener("click", function (e) {
-  if (e.target.classList.contains("gallery-item")) {
-    const src = e.target.getAttribute("src");
-    document.querySelector(".modal-img").src = src;
-    const myModal = new bootstrap.Modal(
-      document.getElementById("gallery-modal")
-    );
-    myModal.show();
+/* modal */
+// Open the Modal
+const openModal = () => {
+  document.getElementById("myModal").style.display = "block";
+};
+
+// Close the Modal
+const closeModal = () => {
+  document.getElementById("myModal").style.display = "none";
+};
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-});
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
 
-const closeImg = document.querySelector(".close-img");
-
-closeImg.addEventListener("click", () => {
-  document.getElementById("gallery-modal").style.display = "none";
-});
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
 
 /* fire base and reservation application */
 
